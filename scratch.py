3fc1814d9bf6b531c7e8d9e1956b7071891986d6
Dustin Bracy <dbracy@smu.edu>
2020-12-17 22:05:00 -0600

    dynamic unique cols

--- scratch.py
+++ scratch.py
@@ -2,12 +2,15 @@
 import pandas as pd
 import numpy as np
 
-#%% Load data:
+# %% Load data:
 data_path = 'E:/20201208_Dementia_AD_Research_David_Julovich/QueryResult/'
 
 # Fix header on encounters:
 new_header = ['person_id','enc_id','place_of_service','Provider_id','EncounterDate','Race','Ethnicity','Gender','AgeAtEnc','VisitType','ServiceDepartment','LocationName','Reason_for_Visit','CPT_Code','CPT_Code_Seq','Encounter_Primary_Payer','Encounter_Secondary_Payer','Encounter_Teritiary_Payer']
 encounters = pd.read_csv(data_path + '1_BaseEncounters_Dempgraphics_Payers.csv', header=None, names=new_header, skiprows=1)
+
+# Drop columns with only one value:
+encounters = encounters.drop(columns=['place_of_service', 'CPT_Code_Seq'])
 
 cpt_codes = pd.read_csv(data_path + '2_CPT_Codes.csv')
 vitals = pd.read_csv(data_path + '3_vitals_signs.csv')
@@ -15,14 +18,10 @@ meds = pd.read_csv(data_path + '4_patient_medication.csv')
 labs = pd.read_csv(data_path + '5_lab_nor__lab_results_obr_p__lab_results_obx.csv')
 diagnoses = pd.read_csv(data_path + '6_patient_diagnoses.csv')
 
-encounters.head()
 
-# %% Look at unique values for each column:
-for col in encounters:
+# %% Look at unique values for each column in a given dataframe:
+df = diagnoses
+for col in df:
     print(col+':\n')
-    print(encounters[col].unique())
+    print(df[col].unique())
 
-# %% Drop columns with only one value
-encounters = encounters.drop(columns=['place_of_service', 'CPT_Code_Seq'])
-
-# %%
