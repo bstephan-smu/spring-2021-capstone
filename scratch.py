6a3e68a459ec7e00f4d89b517219f788f6265e1f
Dustin Bracy <dbracy@smu.edu>
2020-12-21 22:57:12 -0600

    update drops

--- scratch.py
+++ scratch.py
@@ -5,12 +5,10 @@ import numpy as np
 # %% Load data:
 data_path = 'E:/20201208_Dementia_AD_Research_David_Julovich/QueryResult/'
 
+
 # Fix header on encounters:
 new_header = ['person_id','enc_id','place_of_service','Provider_id','EncounterDate','Race','Ethnicity','Gender','AgeAtEnc','VisitType','ServiceDepartment','LocationName','Reason_for_Visit','CPT_Code','CPT_Code_Seq','Encounter_Primary_Payer','Encounter_Secondary_Payer','Encounter_Teritiary_Payer']
 encounters = pd.read_csv(data_path + '1_BaseEncounters_Dempgraphics_Payers.csv', header=None, names=new_header, skiprows=1)
-
-# Drop columns with only one value:
-encounters = encounters.drop(columns=['place_of_service', 'CPT_Code_Seq'])
 
 cpt_codes = pd.read_csv(data_path + '2_CPT_Codes.csv')
 vitals = pd.read_csv(data_path + '3_vitals_signs.csv')
@@ -18,6 +16,9 @@ meds = pd.read_csv(data_path + '4_patient_medication.csv')
 labs = pd.read_csv(data_path + '5_lab_nor__lab_results_obr_p__lab_results_obx.csv')
 diagnoses = pd.read_csv(data_path + '6_patient_diagnoses.csv')
 
+# Drop columns with only one value:
+encounters = encounters.drop(columns=['place_of_service', 'CPT_Code_Seq'])
+vitals = vitals.drop(columns=['age_indicator','bp_comment','pulse_pattern', 'BP'])
 
 # %% Look at unique values for each column in a given dataframe:
 df = diagnoses
@@ -25,3 +26,13 @@ for col in df:
     print(col+':\n')
     print(df[col].unique())
 
+# %%
+dfs = [encounters, cpt_codes, vitals, meds, labs, diagnoses]
+for df in dfs:
+    print(df.isna().sum())
+    print(len(df))
+    print('\n')
+
+# %%
+list(diagnoses.description.unique())
+# %%
