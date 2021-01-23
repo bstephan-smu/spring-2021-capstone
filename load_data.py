e92eaab1b47659cbd67c749897b6d48f39267bf3
Dustin Bracy <dbracy@smu.edu>
2021-01-23 15:03:13 -0600

    colnames before change

--- load_data.py
+++ load_data.py
@@ -24,3 +24,165 @@ AD_people = diagnoses[diagnoses.description.str.contains('Alzh')].person_id.uniq
 AD_encounters = diagnoses[diagnoses.description.str.contains('Alzh')].enc_id.unique()
 
 
+# %% Rename cols: 
+assessments.columns = [
+    'person_id', 
+    'enc_id', 
+    'txt_description', 
+    'diagnosis_code_id',
+    'txt_enc_dx_priority', 
+    'detail_type', 
+    'detail_type_priority'
+    ]
+
+diagnoses.columns = [
+    'person_id', 
+    'enc_id', 
+    'icd9cm_code_id',
+    'diagnosis_code_id',
+    'description', 
+    'date_onset_sympt', 
+    'date_diagnosed', 
+    'date_resolved',
+    'status_id', 
+    'dx_priority', 
+    'chronic_ind', 
+    'recorded_elsewhere_ind'
+]
+
+labs.columns = [
+    'lab_nor_person_id', 
+    'lab_nor_enc_id', 
+    'lab_nor_order_num',
+    'lab_nor_ordering_provider', 
+    'lab_nor_test_location',
+    'lab_nor_sign_off_date', 
+    'lab_nor_test_status', 
+    'lab_nor_ngn_status',
+    'lab_nor_test_desc', 
+    'lab_nor_delete_ind', 
+    'lab_nor_completed_ind',
+    'lab_results_obr_p_seq_num', 
+    'lab_results_obr_p_obs_batt_id',
+    'lab_results_obr_p_producer_field_1', 
+    'lab_results_obr_p_test_desc',
+    'lab_results_obr_p_ng_test_desc', 
+    'lab_results_obr_p_coll_date_time',
+    'lab_results_obr_p_spec_rcv_date_time', 
+    'lab_results_obx_observ_value',
+    'lab_results_obx_units', 
+    'lab_results_obx_ref_range',
+    'lab_results_obx_abnorm_flags', 
+    'lab_results_obx_observ_result_stat',
+    'lab_results_obx_obs_date_time', 
+    'lab_results_obx_result_desc',
+    'lab_results_obx_delete_ind', 
+    'lab_results_obx_result_comment'
+    ]
+
+meds.columns = [
+    'lab_nor_person_id', 
+    'lab_nor_enc_id', 
+    'lab_nor_order_num',
+    'lab_nor_ordering_provider', 
+    'lab_nor_test_location',
+    'lab_nor_sign_off_date', 
+    'lab_nor_test_status', 
+    'lab_nor_ngn_status',
+    'lab_nor_test_desc', 
+    'lab_nor_delete_ind', 
+    'lab_nor_completed_ind',
+    'lab_results_obr_p_seq_num',
+    'lab_results_obr_p_obs_batt_id',
+    'lab_results_obr_p_producer_field_1', 
+    'lab_results_obr_p_test_desc',
+    'lab_results_obr_p_ng_test_desc', 
+    'lab_results_obr_p_coll_date_time',
+    'lab_results_obr_p_spec_rcv_date_time', 
+    'lab_results_obx_observ_value',
+    'lab_results_obx_units', 
+    'lab_results_obx_ref_range',
+    'lab_results_obx_abnorm_flags', 
+    'lab_results_obx_observ_result_stat',
+    'lab_results_obx_obs_date_time', 
+    'lab_results_obx_result_desc',
+    'lab_results_obx_delete_ind', 
+    'lab_results_obx_result_comment'
+    ]
+
+vitals.columns = [
+    'person_id', 
+    'enc_id', 
+    'seq_no', 
+    'age_cal', 
+    'BMI_calc',
+    'bp_body_position_2', 
+    'bp_cuff_size', 
+    'bp_diastolic', 
+    'bp_systolic',
+    'BP_target_side', 
+    'BP_target_site', 
+    'bp_target_site_cd', 
+    'comments',
+    'height_cm', 
+    'height_in', 
+    'height_date', 
+    'pulse_pattern_2',
+    'pulse_rate', 
+    'respiration_rate', 
+    'temp_deg_F', 
+    'temp_targt_site_cd',
+    'vitalSignsDate', 
+    'vitalSignsTime', 
+    'weight_context', 
+    'weight_lb',
+    'painScoreDisplay'
+    ]
+
+cpt_codes.columns = [
+    'enc_id', 
+    'CPT_Code', 
+    'CPT_Code_Seq'
+    ]
+
+encounters.columns = [
+    'person_id', 
+    'enc_id',
+    'Provider_id',
+    'EncounterDate', 
+    'Race',
+    'Ethnicity', 
+    'Gender',
+    'AgeAtEnc', 
+    'VisitType', 
+    'ServiceDepartment',
+    'LocationName', 
+    'Reason_for_Visit',
+    'CPT_Code',
+    'Encounter_Primary_Payer',
+    'Encounter_Secondary_Payer',
+    'Encounter_Teritiary_Payer'
+    ]    
+
+
+
+
+# %%
+assessments.head()
+
+# %%
+diagnoses.head()
+
+# %%
+# begin merge
+df = assessments.merge(
+    diagnoses, 
+    how='inner', 
+    left_on=['person_id','enc_id','txt_diagnosis_code_id'], 
+    right_on=['person_id','enc_id','diagnosis_code_id']
+)
+# %%
+len(assessments)
+
+# %%
+len(df)
