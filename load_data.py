3db9db2c9d0e2b09c79925e4a12004296fb6f092
Dustin Bracy <dbracy@smu.edu>
2021-02-10 08:02:26 -0600

    patch dataloader

--- load_data.py
+++ load_data.py
@@ -710,7 +710,10 @@ class DataLoader:
 
         # step 6...load merged assessments + diagnoses onto main dataframe
         print('encoding assessments')
+
         self.format_assessments()
+
+        # Start patch here: everything up to this point was good
         self.merge_assessments()
 
         # step 7...clean data: drop NAs, rename cols etc
@@ -751,4 +754,56 @@ if __name__ == "__main__":
     #data.create()
     print(data.load())
 
+# %% DO NOT RUN!!!
+# 2/10/21 PATCH_1: fix for failed loader during merge_assessments
+# this just runs merge_assessments manually, with a patch to handle NAs inside the CCSR lists 
+
+
+assess_copy = capData.asmt_diag.copy()
+
+# Pair down assessments table to columns of interest
+assess_copy = assess_copy[
+    ['person_id', 'enc_id', 'txt_description', 'txt_tokenized', 'ngrams', 'ngram2', 'txt_tokenized2',
+        'np_chunks', 'CCSR Category', 'description']]
+
+
+
+def one_hot(df, col_name, prefix=''):
+    mlb = MultiLabelBinarizer()
+    df = df.join(pd.DataFrame(mlb.fit_transform(df[col_name]), columns=prefix + mlb.classes_))
+    df = df.drop(columns=[col_name])
+    return df
+
+
+def stripNA(lst_col):
+    for item in lst_col:
+        if type(item) != str:
+            lst_col.remove(item)
+    return lst_col
+
+assess_copy['CCSR Category'] = assess_copy['CCSR Category'].apply(stripNA)
+assess_encoded = one_hot(assess_copy, 'CCSR Category', prefix='ccsr_')
+
+assess_encoded.rename(columns={
+    'txt_description': 'asmt_txt_description',
+    'txt_tokenized': 'asmt_txt_tokenized',
+    'ngrams': 'asmt_ngrams',
+    'ngram2': 'asmt_ngram2',
+    'txt_tokenized2': 'asmt_txt_tokenized2',
+    'np_chunks': 'asmt_np_chunks',	
+    'description': 'asmt_description'
+}, inplace=True)
+
+capData.main = capData.main.merge(assess_copy, on=['person_id', 'enc_id'], how='left')
+
+capData.encode_encounters()
+
+capData.clean()
+
+capData.write()
+
+# %%
+from load_data import DataLoader
+tmp = DataLoader().load()
+tmp.main
 # %%
